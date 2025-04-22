c168f57cd6c898fcd332c5038685cfd2e4d18bd2
Manisha Patwal <manishapatwal34@gmail.com>
2025-04-22 22:15:18 +0530

    modified

--- Pandas_library/basic_intro.py
+++ Pandas_library/basic_intro.py
@@ -1,4 +1,9 @@
 import pandas as pd
 f_t = pd.read_csv(
     "C:/Users/Monalisha/OneDrive/Desktop/mca_prog/python/Pandas_library/market_fact.csv")
-print(f_t.head())
+print(f_t.head())  # will retrieve first 5 rows
+print(f_t.tail())
+print(f_t.info())
+print(f_t.describe())
+f_t.dropna()
+print(f_t.max())
